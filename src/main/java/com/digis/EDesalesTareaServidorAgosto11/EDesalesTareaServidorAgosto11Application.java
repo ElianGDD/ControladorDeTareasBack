9851c6c84594c1bdab4fbfbecbf58d71629ee609
Alien 13 <Alien 13@ALIEN13>
2025-08-11 17:32:18 -0600

    Comienzo Proyecto

---
+++ src/main/java/com/digis/EDesalesTareaServidorAgosto11/EDesalesTareaServidorAgosto11Application.java
@@ -0,0 +1,13 @@
+package com.digis.EDesalesTareaServidorAgosto11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDesalesTareaServidorAgosto11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDesalesTareaServidorAgosto11Application.class, args);
+	}
+
+}
